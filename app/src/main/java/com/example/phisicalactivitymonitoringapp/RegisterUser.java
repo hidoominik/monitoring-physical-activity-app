bb19c872b92cab5bf55834e73d6354407ba41a3b
hidoominik <dominik.choma.41@gmail.com>
2022-10-14 12:30:10 +0200

    Register new user to firebase database, via firebase auth

---
+++ app/src/main/java/com/example/phisicalactivitymonitoringapp/RegisterUser.java
@@ -0,0 +1,121 @@
+package com.example.phisicalactivitymonitoringapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+    private TextView banner, registerUser;
+    private EditText editTextEmail, editTextUsername, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextUsername = (EditText) findViewById(R.id.username);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+        }
+
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Provide valid email.");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(username.isEmpty()){
+            editTextUsername.setError("Username is required");
+            editTextUsername.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){ //Firebase requirements, password.length >= 6
+            editTextPassword.setError("Password is length should minimum 6 characters.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    User user = new User(username, email);
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())//id of registered user
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() { //sth wrong here, check connectron to realdatabase, sth with google.services.json(downloadable via firebase website->project)
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(RegisterUser.this, "User has been registered successfully", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }else{
+                                        Toast.makeText(RegisterUser.this, "Something went wrong...", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                }else{
+                    Toast.makeText(RegisterUser.this, "Something went wrong...", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
