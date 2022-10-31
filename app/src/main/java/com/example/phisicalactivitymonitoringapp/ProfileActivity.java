f1bb51497f2c9334d9d434459d325553066e25e1
hidoominik <dominik.choma.41@gmail.com>
2022-10-31 11:50:05 +0100

    logout function

--- app/src/main/java/com/example/phisicalactivitymonitoringapp/ProfileActivity.java
+++ app/src/main/java/com/example/phisicalactivitymonitoringapp/ProfileActivity.java
@@ -2,13 +2,27 @@ package com.example.phisicalactivitymonitoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private Button logoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logoutButton =(Button) findViewById(R.id.signOut);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
     }
 }
