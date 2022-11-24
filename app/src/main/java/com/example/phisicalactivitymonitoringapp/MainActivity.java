b85e3e42e1aa4c395956e43968ea4130aaf2e2ae
g.bielesza <grzegorz.bielesza@infinite.pl>
2022-11-24 20:21:26 +0100

    Navigation menu fix for MainActivity

--- app/src/main/java/com/example/phisicalactivitymonitoringapp/MainActivity.java
+++ app/src/main/java/com/example/phisicalactivitymonitoringapp/MainActivity.java
@@ -2,46 +2,93 @@ package com.example.phisicalactivitymonitoringapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.phisicalactivitymonitoringapp.authorization.UserLoginActivity;
 import com.example.phisicalactivitymonitoringapp.authorization.services.AuthService;
+import com.example.phisicalactivitymonitoringapp.user.UserProfileActivity;
+import com.google.android.material.navigation.NavigationView;
 
 //User do testowania
 //email: chomczaq@gmail.com
 //password: asdf77
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button signOut;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        signOut = (Button) findViewById(R.id.signOutMainActivityButton);
-        signOut.setOnClickListener(this);
-
+    @Override
+    protected void onStart() {
+        super.onStart();
         if(!(AuthService.ifUserIsLoggedIn())){
             startActivity(new Intent(MainActivity.this, UserLoginActivity.class));
         }
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.signOutMainActivityButton:
-                signOut();
-                break;
-        }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        if(getSupportActionBar() !=null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //TODO: Trzeba pomyśleć jak byśmy chcieli to nav menu dodawać do innych aktywności, dziedziczymy chamsko po MainActivity, czy może jakiś lepszy sposob na wstrzykiwanie tego menu?
+        //Navigation logic:
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.nav_home:
+                    Log.i("MENU_DRAWER_TAG","Home item clicked");
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    break;
+                case R.id.nav_search:
+                    Log.i("MENU_DRAWER_TAG","Search item clicked");
+                    //logic for search
+                    break;
+
+                case R.id.nav_logout:
+                    Log.i("MENU_DRAWER_TAG","Logout item clicked");
+                    signOut();
+                    break;
+            }
+            return true;
+        });
+
     }
 
     private void signOut(){
         AuthService.signOut();
         finishAffinity();
         startActivity(getIntent());
+    }
+
+    @Override
+    public void onClick(View view) {
+//    Na pewno przyda się onclick ;)
     }
 }
