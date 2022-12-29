90ef4d4ad027e3d7dce30e090870927714efda01
kingachwaleba <kingach6@gmail.com>
2022-12-29 18:57:59 +0100

    Show an username in the profile

--- app/src/main/java/com/example/phisicalactivitymonitoringapp/user/UserProfileActivity.java
+++ app/src/main/java/com/example/phisicalactivitymonitoringapp/user/UserProfileActivity.java
@@ -1,26 +1,59 @@
 package com.example.phisicalactivitymonitoringapp.user;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.phisicalactivitymonitoringapp.R;
+import com.example.phisicalactivitymonitoringapp.user.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private TextView usernameTextView;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+        usernameTextView = findViewById(R.id.username);
+
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
-        Toast.makeText(this, currentUser.getEmail(), Toast.LENGTH_SHORT).show();
+        if (currentUser != null) {
+            Query emailQuery = mDatabase.orderByChild("email").equalTo(currentUser.getEmail());
+            emailQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot child : snapshot.getChildren()) {
+                        User user = child.getValue(User.class);
+                        if (user != null)
+                            usernameTextView.setText(user.getUsername());
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.d("TAG", error.getMessage());
+                }
+            });
+        }
     }
 }
