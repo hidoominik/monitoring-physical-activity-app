2dc1f9478b7c31783f35c02ce3d33b9ddd31a326
kingachwaleba <kingach6@gmail.com>
2023-01-20 21:41:18 +0100

    Show user's stats

--- app/src/main/java/com/example/phisicalactivitymonitoringapp/workouts/ShowStatsActivity.java
+++ app/src/main/java/com/example/phisicalactivitymonitoringapp/workouts/ShowStatsActivity.java
@@ -1,10 +1,13 @@
 package com.example.phisicalactivitymonitoringapp.workouts;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.phisicalactivitymonitoringapp.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +19,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class ShowStatsActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
@@ -23,6 +33,11 @@ public class ShowStatsActivity extends AppCompatActivity {
     private String anotherUserUsername;
     private DatabaseReference users;
     private DatabaseReference workouts;
+
+    private TextView allWorkoutsNumber;
+    private TextView firstWorkoutDate;
+    private TextView lastWorkoutDate;
+    private TextView longestWorkoutTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +53,11 @@ public class ShowStatsActivity extends AppCompatActivity {
                 ? getIntent().getSerializableExtra("username").toString()
                 : null;
 
+        allWorkoutsNumber = (TextView) findViewById(R.id.all_workouts);
+        firstWorkoutDate = (TextView) findViewById(R.id.first_workout);
+        lastWorkoutDate = (TextView) findViewById(R.id.last_workout);
+        longestWorkoutTime = (TextView) findViewById(R.id.max_workout_time);
+
         getStats();
     }
 
@@ -45,18 +65,45 @@ public class ShowStatsActivity extends AppCompatActivity {
         if (currentUser != null) {
             String usernameForShowUserDetails = anotherUserUsername != null ?
                     anotherUserUsername : currentUser.getDisplayName();
-            System.out.println("AAAAA " + usernameForShowUserDetails);
-            Query workoutQuery = workouts.orderByChild("username").equalTo(usernameForShowUserDetails);
 
-//            Query emailQuery = users.orderByChild("email").equalTo(currentUser.getEmail());
+            Query workoutQuery = workouts.orderByChild("username").equalTo(usernameForShowUserDetails);
             workoutQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    List<Workout> workoutList = new ArrayList<>();
                     for (DataSnapshot child : snapshot.getChildren()) {
                         Workout workout = child.getValue(Workout.class);
                         if (workout != null) {
-                            System.out.println(workout.getName());
+                            workoutList.add(child.getValue(Workout.class));
                         }
+                    }
+
+                    if (workoutList.size() != 0) {
+                        workoutList.sort(Comparator.comparing(Workout::getDate));
+
+                        allWorkoutsNumber.setText(String.valueOf(workoutList.size()));
+                        firstWorkoutDate.setText(String.valueOf(workoutList.get(0).getDate()));
+                        lastWorkoutDate.setText(
+                                String.valueOf(workoutList.get(workoutList.size() - 1).getDate()));
+
+                        List<Long> workoutsTime = new ArrayList<>();
+                        for (Workout w : workoutList) {
+                            LocalTime startTime = LocalTime.parse(w.getStartTime());
+                            LocalTime endTime = LocalTime.parse(w.getEndTime());
+
+                            long diffInMinutes = Duration.between(startTime, endTime).toMinutes();
+
+                            workoutsTime.add(diffInMinutes);
+                        }
+
+                        longestWorkoutTime.setText(String.valueOf(Collections.max(workoutsTime)));
+                    }
+                    else {
+                        allWorkoutsNumber.setText(R.string.no_data_info);
+                        firstWorkoutDate.setText(R.string.no_data_info);
+                        lastWorkoutDate.setText(R.string.no_data_info);
+                        longestWorkoutTime.setText(R.string.no_data_info);
                     }
                 }
 
